ce6e71e9b25631be4feaef7e2fdde1d3ff7c5cd5
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 15:40:48 +0700

    Adding src/CompilerPassInterface.php

---
+++ src/CompilerPassInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Container;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+interface CompilerPassInterface
+{
+	/**
+	 * Invoke current class to inject dependency
+	 * into given container.
+	 *
+	 * @param ContainerBuilderInterface $container Dependency injection container.
+	 * @return void
+	 */
+	public function __invoke(ContainerBuilderInterface $container);
+
+	/**
+	 * Get service tag name.
+	 *
+	 * @return string
+	 */
+	public function getTag();
+
+	/**
+	 * Get associated class name.
+	 *
+	 * @return string
+	 */
+	public function getAssociatedClass();
+
+	/**
+	 * Get serialized service instantiation.
+	 *
+	 * @return string
+	 */
+	public function getSerializedValue();
+}
