94887c1c4c16b8d31554df4d449fcadd3924e51c
plvhx <gandung@slackerc0de.us>
2020-07-28 03:09:30 +0700

    Refactoring src/Container.php

--- src/Container.php
+++ src/Container.php
@@ -13,23 +13,28 @@ class Container implements ContainerInterface
     /**
      * @var array
      */
-    protected $instances = [];
-
-    /**
-     * @var array
-     */
-    private $resolved = [];
-
-    /**
-     * @var array
-     */
     private $cached = [];
+
+    /**
+     * @var array
+     */
+    private $definitions = [];
+
+    /**
+     * @var array
+     */
+    private $instances = [];
+
+    /**
+     * @var array
+     */
+    private $resolvedDefinitions = [];
 
     public function __construct($definitions = [], $instances = [], $cached = [])
     {
-        $this->setDefinitions($definitions);
-        $this->setCachedEntries($cached);
-        $this->setInstances($instances);
+        $this->cached      = $cached;
+        $this->definitions = $definitions;
+        $this->instances   = $instances;
     }
 
     /**
@@ -54,14 +59,19 @@ class Container implements ContainerInterface
      */
     public function has($id)
     {
-        $keys = array_unique(
-            array_merge(
-                array_keys($this->cached),
-                array_keys($this->instances)
-            )
-        );
+        if (!isset($this->cached[$id])) {
+            return false;
+        }
 
-        return in_array($id, $keys, true);
+        if (!isset($this->definitions[$id])) {
+            return false;
+        }
+
+        if (!isset($this->instances[$id])) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -70,5 +80,83 @@ class Container implements ContainerInterface
      * @param string $id Definition ID.
      * @return object
      */
-    abstract protected function make($id);
+    private function make($id)
+    {
+        if (isset($this->cached[$id])) {
+            return $this->cached[$id];
+        }
+
+        if (isset($this->instances[$id])) {
+            return $this->instances[$id];
+        }
+
+        return isset($this->resolvedDefinitions[$id])
+            ? $this->resolvedDefinitions[$id]
+            : $this->resolveDefinition($id);
+    }
+
+    /**
+     * Resolve chosen definition by it's ID.
+     *
+     * @param string $id Definition ID.
+     * @return object
+     */
+    private function resolveDefinition($id)
+    {
+        $definition = $this->definitions[$id];
+        $class      = $definition->getClass();
+        $arguments  = $definition->getArguments();
+
+        if ($definition->isAutowired()) {
+            return $this->resolveAutowiredClass($class);
+        }
+
+        for ($i = 0; $i < sizeof($arguments); $i++) {
+            $arguments[$i] = ($arguments[$i] instanceof Reference)
+                ? $this->make($arguments[$i]->getId())
+                : $arguments[$i];
+        }
+
+        return $this->resolvedDefinitions[$id] = (new ReflectionClass($class))->newInstanceArgs($arguments);
+    }
+
+    /**
+     * Resolve class with autowiring configuration.
+     *
+     * @param string $class Class name.
+     * @return object
+     */
+    private function resolveAutowiredClass($class)
+    {
+        $reflection = new ReflectionClass($class);
+
+        if (!$reflection->hasMethod('__construct')) {
+            return $reflection->newInstanceWithoutConstructor();
+        }
+
+        $constructorMethod = $reflection->getConstructor();
+
+        if ($constructorMethod->getNumberOfParameters() === 0) {
+            return $reflection->newInstanceArgs([]);
+        }
+
+        $constructorMethodParams = [];
+
+        foreach ($constructorMethod->getParameters() as $param) {
+            $class = $param->getClass();
+
+            if ($class === null) {
+                continue;
+            }
+
+            if ($class->isInterface()) {
+                $constructorMethodParams[] = $this->getAlias($class->getName());
+                continue;
+            }
+
+            $constructorMethodParams[] = $this->resolveAutowiredClass($class->getName());
+        }
+
+        return $reflection->newInstanceArgs($constructorMethodParams);
+    }
 }
